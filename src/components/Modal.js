a1e0c52c009b33e7d4e1f253e3dbefb82c8e0fc2
Justice Ampofo <justiceampofo@gmail.com>
2023-07-01 15:29:03 -0400

    remove comments and add content to the homepage to test scrolling while the modal is active

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,14 +4,12 @@ function Modal({ closeModal }) {
     return (
         <>
             <div onClick={closeModal} className="modal__dimmer"></div>
-            {/* <div className='modal'> */}
-                <div className="modal__container">
-                    <button onClick={closeModal} className='btn modal__btn-close'>Close</button>
-                    <h2 className='h2 mb5'>My modal<br className='mobile-only-block' /> content here</h2>
-                    <p className="mb5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel voluptatem eos doloribus cum doloremque omnis deserunt recusandae architecto dicta reprehenderit.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, saepe.</p>
-                </div>
-            {/* </div> */}
+            <div className="modal__container">
+                <button onClick={closeModal} className='btn modal__btn-close'>Close</button>
+                <h2 className='h2 mb5'>My modal<br className='mobile-only-block' /> content here</h2>
+                <p className="mb5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel voluptatem eos doloribus cum doloremque omnis deserunt recusandae architecto dicta reprehenderit.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, saepe.</p>
+            </div>
         </>
     )
 }
