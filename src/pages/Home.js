4562e1d0466506734a1b4ac0d3115508fd3d522e
Justice Ampofo <justiceampofo@gmail.com>
2023-07-01 15:15:35 -0400

    start v2 development

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
+import { useState } from "react"
+import Modal from "../components/Modal"
+
 function Home() {
+    const [modalAOpen, setModalAOpen] = useState(false)
+
     return (
-        <div className="container">
-            <p className="mb5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel voluptatem eos doloribus cum doloremque omnis deserunt recusandae architecto dicta reprehenderit.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, saepe.</p>
-        </div>
+        <>
+            <div className="container">
+                <p className="mb5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel voluptatem eos doloribus cum doloremque omnis deserunt recusandae architecto dicta reprehenderit.</p>
+                <button onClick={() => {setModalAOpen(true)}} className="mb5">Open Modal</button>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, saepe.</p>
+            </div>
+            {modalAOpen && <Modal closeModal={() => setModalAOpen(false)} />}
+        </>
     )
 }
 export default Home
