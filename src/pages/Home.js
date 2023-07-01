c4ad9649df3219da2b6cc4a03bd4370302e47d3e
Justice Ampofo <justiceampofo@gmail.com>
2023-07-01 16:16:47 -0400

    add styles to better match the design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import Modal from "../components/Modal"
+import "../styles/Home.css"
 
 function Home() {
     const [modalAOpen, setModalAOpen] = useState(false)
@@ -15,9 +16,9 @@ function Home() {
 
     return (
         <>
-            <div className="container">
+            <div className="container home">
                 <p className="mb5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vitae laoreet ligula. Quisque tempor nunc vel ante rutrum, ut mollis ipsum scelerisque. Sed in consectetur erat. Quisque neque leo, efficitur et pretium at, elementum in augue. Aenean vel ex pretium, sodales dolor sit amet, varius massa. Donec gravida sapien purus. Nullam volutpat faucibus fermentum. Nam et tortor id massa vestibulum tincidunt. Vivamus at diam vel nisl ornare ullamcorper at quis neque. Aliquam sit amet neque facilisis sem bibendum porta. Nullam sed dapibus leo, scelerisque egestas purus. In ut velit eget ex dapibus mollis eget et sapien. Quisque consequat lacus a ipsum porta, eget fermentum purus elementum.</p>
-                <button onClick={() => {setModalAOpen(true)}} className="mb5">Open Modal</button>
+                <button onClick={() => {setModalAOpen(true)}} className="btn mb5">Open Modal</button>
                 <p className="mb5">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus ornare tortor dignissim, viverra tellus vitae, cursus risus. Curabitur accumsan augue tempus, molestie massa quis, rhoncus nibh. Integer auctor neque eu elit vulputate malesuada. Proin id mattis risus, ut dictum ligula. Morbi eget tristique dui. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras ac volutpat leo. Maecenas cursus sapien eget leo aliquam bibendum. Sed porta ante ante, in varius erat interdum eget. Proin finibus diam quis aliquet ultricies. Etiam sed velit id eros laoreet congue sit amet non magna. Sed in dapibus nisl. Cras velit orci, viverra a diam eget, lacinia pulvinar eros.</p>
                 <p className="mb5">Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla euismod nunc arcu, in venenatis risus fermentum non. Duis blandit nulla eu arcu consequat, vel malesuada nibh varius. Aenean consectetur interdum metus eu sagittis. Nulla sit amet leo in quam lacinia malesuada. Suspendisse potenti. Sed nec turpis eu enim ornare auctor non id augue. Integer consectetur porta turpis sit amet finibus. Cras rhoncus pharetra magna nec mollis.</p>
                 <p className="mb5">Mauris sed mauris sapien. Donec diam ex, ultrices vel blandit vel, porta nec nisi. Nulla ornare nibh orci, non lobortis arcu elementum a. Aliquam neque ex, hendrerit id eros eu, pharetra gravida nisi. In hac habitasse platea dictumst. Sed commodo mauris sit amet justo tincidunt, mattis mollis eros laoreet. In justo leo, tincidunt nec sagittis quis, commodo at ligula. Duis consectetur dictum orci.</p>
