5cc70a711229fb6417c3a5c6b4305bd5ad73ff86
Justice Ampofo <justiceampofo@gmail.com>
2023-07-05 08:59:05 -0400

    add close button to the safety bar component and apply header fixes

--- src/components/SafetyBar.js
+++ src/components/SafetyBar.js
@@ -1,11 +1,18 @@
 import "../styles/SafetyBar.css"
 
 function SafetyBar() {
+    const scrollToISI = () => {
+        console.log('scroll to ISI clicked');
+    }
+
     return (
         <div className="safety-bar container">
-            <h2 className="h1 mb5">IMPORTANT SAFETY INFORMATION</h2>
-            <p className="mb5">3 visible lines of copy should always be visible</p>
-            <p className="mb5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras suscipit, metus ac hendrerit vestibulum, turpis tortor tempus lacus, nec mollis magna lorem ac nisi. Vivamus quis enim lorem. Nam finibus eu tellus sed tempor. Pellentesque odio erat, condimentum nec sem nec, tempor sollicitudin lectus. In tortor elit, lobortis nec lorem vel, ultricies faucibus eros. Aliquam quis tellus a orci vestibulum semper. Etiam interdum feugiat nisl, et lobortis est porttitor quis. Donec quis lacinia nibh. In eget gravida arcu, et iaculis est. Morbi ut lectus at mi commodo fermentum eu id ante.</p>
+            <div className="safety-bar__content">
+                <button onClick={scrollToISI} className='btn safety__btn-isi'>+</button>
+                <h2 className="h1 mb5">IMPORTANT SAFETY INFORMATION</h2>
+                <p className="mb5">3 visible lines of copy should always be visible</p>
+                <p className="mb5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras suscipit, metus ac hendrerit vestibulum, turpis tortor tempus lacus, nec mollis magna lorem ac nisi. Vivamus quis enim lorem.</p>
+            </div>
         </div>
     )
 }
