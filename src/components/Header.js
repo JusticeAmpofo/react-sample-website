e222bfff6457a0e66074ad644994c8b429f4650a
Justice Ampofo <justiceampofo@gmail.com>
2023-08-09 21:58:48 -0400

    apply fix for mobile nav to collapse when user naigates to a new page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import navLinks from '../content/navLinks';
 import '../styles/Header.css';
 
 function Header() {
     const [expandNav, setExpandNav] = useState(false);
+
+    let location = useLocation();
+
+    useEffect(() => {
+        setExpandNav(false);
+    }, [location.pathname]);
 
     return (
         <header className='header website-border'>
